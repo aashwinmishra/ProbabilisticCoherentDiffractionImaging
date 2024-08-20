ccb3a35852581d90426fb64c7da41b4f9f37d7af
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-08-20 01:27:47 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -56,3 +56,34 @@ def create_summary_writer(experiment_name: str, model_name: str, extras: str = N
   writer = torch.utils.tensorboard.SummaryWriter(log_dir)
   return writer
 
+
+def predict_and_plot(model: torch.nn.Module, 
+                     test_dl: torch.utils.data.DataLoader, 
+                     device: torch.device):
+  """
+  Takes the test data loader and a probabilistic model, predicts for entire data.
+  For a subsample, plots the predictions.
+  Args:
+    model: Trained probabilistic model (PNN, Deep Ensemble, etc)
+    test_dl: Data Loader for the test data
+    device: Device of the model
+  Returns:
+    tuple with amps_mean, amps_var, phis_mean, phis_var
+  """
+  model.eval() #just to be safe
+  amps_mean, amps_var, phis_mean, phis_var = [], [], [], []
+  for i, ft_images in enumerate(test_dl):
+    ft_images = ft_images[0].to(device)
+    amp_mean, amp_var, phi_mean, phi_var = model(ft_images)
+    for j in range(ft_images.shape[0]):
+      amps_mean.append(amp_mean[j].detach().to("cpu").numpy())
+      amps_var.append(amp_var[j].detach().to("cpu").numpy())
+      phis_mean.append(phi_mean[j].detach().to("cpu").numpy())
+      phis_var.append(phi_var[j].detach().to("cpu").numpy())
+
+  amps_mean = np.array(amps_mean).squeeze()
+  amps_var = np.array(amps_var).squeeze()
+  phis_mean = np.array(phis_mean).squeeze()
+  phis_var = np.array(phis_var).squeeze()
+
+  return amps_mean, amps_var, phis_mean, phis_var
