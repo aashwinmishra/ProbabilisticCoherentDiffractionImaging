7082ba5477007c58b0e91e0f14a6b704d8080761
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-08-22 19:01:42 -0700

    Update train.py

--- train.py
+++ train.py
@@ -8,26 +8,26 @@ import torchvision.transforms as transforms
 import os
 import argparse
 from data_setup import get_dataloaders
-from models import PtychoNN
+from models import PtychoNN, PtychoPNN, DeepEnsemble
 from engine import train
 from utils import get_devices, set_seeds, save_model
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", type=str, default="./gdrive/MyDrive/PtychoNNData/")
-parser.add_argument("--num_epochs", type=int, default=75)
+parser.add_argument("--num_epochs", type=int, default=125)
 parser.add_argument("--lr", type=float, default=0.001)
 parser.add_argument("--batch_size", type=int, default=64)
-parser.add_argument("--model", type=str, default="PtychoNNBase")
+parser.add_argument("--model", type=str, default="PtychoModel")
 args = parser.parse_args()
 
 set_seeds(42)
 device = get_devices()
 d = get_dataloaders(args.data_path)
 train_dl, val_dl, test_dl = d["train_dl"], d["val_dl"], d["test_dl"]
-model = PtychoNN().to(device)
+model = PtychoPNN().to(device)
 opt = torch.optim.Adam(model.parameters(), lr=args.lr)
 results = train(model, train_dl, val_dl, opt, device, args.num_epochs)
 model_name = args.model + str(args.num_epochs)
-save_model("./Models", "model_" + args.model, model)
+save_model("./Models", model_name, model)
 
