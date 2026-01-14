ec1ba296855cfddf26012ddd7874cf1bedb70fa3
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2026-01-13 19:35:43 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -170,56 +170,7 @@ def evaluate(model: torch.nn.Module,
     print("Amp MSE:", amps_mse.detach().item())
     print("Phi MSE:", phis_mse.detach().item())
     return amps_mae.detach().item(), amps_mse.detach().item(), phis_mae.detach().item(), phis_mse.detach().item()
-
-
-def load_ptycho_data(file_path: str) -> PtychoDataContainer:
-    """
-    Load the npz-serialized ptycho data.
-
-    Args:
-        file_path (str): Path to the npz file.
-
-    Returns:
-        PtychoDataContainer: Loaded ptycho data.
-    """
-    data = np.load(file_path, allow_pickle=True)
-    return PtychoDataContainer(
-        X=data['X'],
-        Y_I=data['Y_I'],
-        Y_phi=data['Y_phi'],
-        norm_Y_I=data['norm_Y_I'],
-        YY_full=data['YY_full'],
-        coords_nominal=data['coords_nominal'],
-        coords_true=data['coords_true'],
-        nn_indices=data['nn_indices'],
-        global_offsets=data['global_offsets'],
-        local_offsets=data['local_offsets'],
-        probeGuess=data['probe']
-    )
-
-
-def inspect_ptycho_frames(data: PtychoDataContainer, 
-                          num_frames: int = 2):
-    """
-    Visually inspect a couple of frames from X, Y_I, and Y_phi.
-    Args:
-        data (PtychoDataContainer): Loaded ptycho data.
-        num_frames (int): Number of frames to display. Defaults to 2.
-    """
-    fig, axes = plt.subplots(3, num_frames, figsize=(5*num_frames, 15))
-    for i in range(num_frames):
-        axes[0, i].imshow(data.X[i, ..., 0], cmap='viridis')
-        axes[0, i].set_title(f'X - Frame {i}')
-        axes[0, i].axis('off')
-        axes[1, i].imshow(data.Y_I[i, ..., 0], cmap='viridis')
-        axes[1, i].set_title(f'Y_I - Frame {i}')
-        axes[1, i].axis('off')
-        axes[2, i].imshow(data.Y_phi[i, ..., 0], cmap='viridis')
-        axes[2, i].set_title(f'Y_phi - Frame {i}')
-        axes[2, i].axis('off')
-    plt.tight_layout()
-    plt.show()
-
+               
 
 def mc_predictions(model: nn.Module, 
                    ft_images: torch.tensor, 
