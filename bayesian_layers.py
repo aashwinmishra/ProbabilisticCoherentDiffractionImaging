9e003173d8102bbfd2d97d61fb17cd0795da31dc
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2026-02-02 11:41:48 -0800

    Update bayesian_layers.py

--- bayesian_layers.py
+++ bayesian_layers.py
@@ -20,7 +20,102 @@ class Gaussian:
     return ( -0.5 * torch.log(2 * torch.pi * self.sigma**2) - 0.5 * ((x - self.mu) / self.sigma)**2).sum()
 
 
-class BBBLayer(nn.Module):
+class BBBConv2d(nn.Module):
+  def __init__(self, 
+               in_channels: int, 
+               out_channels: int, 
+               kernel_size: int, 
+               stride: int=1, 
+               padding: int=0):
+    super().__init__()
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+    self.kernel_size = kernel_size
+    self.stride = stride
+    self.padding = padding
+
+    weight_mu = torch.empty(out_channels, in_channels, kernel_size, kernel_size)
+    nn.init.kaiming_uniform_(weight_mu, nonlinearity='relu')
+    self.weight_mu = nn.Parameter(weight_mu)
+    self.weight_rho = nn.Parameter(torch.Tensor(out_channels, in_channels, kernel_size, kernel_size).uniform_(-5, -4))
+    self.weight = Gaussian(self.weight_mu, self.weight_rho)
+
+    bias_mu = torch.empty(out_channels,)
+    nn.init.constant_(bias_mu, 0.0)
+    self.bias_mu = nn.Parameter(bias_mu)
+    self.bias_rho = nn.Parameter(torch.Tensor(out_channels,).uniform_(-5, -4))
+    self.bias = Gaussian(self.bias_mu, self.bias_rho)
+
+    self.weight_prior = Gaussian(torch.zeros_like(self.weight_mu), torch.ones_like(self.weight_rho)*0.542)
+    self.bias_prior = Gaussian(torch.zeros_like(self.bias_mu), torch.ones_like(self.bias_rho)*0.542)
+
+    self.log_prior = 0.0
+    self.log_variational_posterior = 0.0
+
+  def forward(self, x: torch.tensor, sample: bool=False)->torch.tensor:
+    if self.training or sample:
+      weight = self.weight.sample()
+      bias = self.bias.sample()
+    else:
+      weight = self.weight.mu
+      bias = self.bias.mu
+    if self.training:
+      self.log_prior = self.weight_prior.log_prob(weight) + self.bias_prior.log_prob(bias)
+      self.log_variational_posterior = self.weight.log_prob(weight) + self.bias.log_prob(bias)
+    else:
+      self.log_prior, self.log_variational_posterior = 0.0, 0.0
+    return F.conv2d(x, weight, bias, self.stride, self.padding)
+
+
+class BBBConv2d_Output(nn.Module):
+  def __init__(self, 
+               in_channels: int, 
+               out_channels: int, 
+               kernel_size: int, 
+               stride: int=1, 
+               padding: int=0):
+    super().__init__()
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+    self.kernel_size = kernel_size
+    self.stride = stride
+    self.padding = padding
+
+    weight_mu = torch.empty(out_channels * 2, in_channels, kernel_size, kernel_size)
+    nn.init.kaiming_uniform_(weight_mu, nonlinearity='relu')
+    self.weight_mu = nn.Parameter(weight_mu)
+    self.weight_rho = nn.Parameter(torch.Tensor(out_channels * 2, in_channels, kernel_size, kernel_size).uniform_(-5, -4))
+    self.weight = Gaussian(self.weight_mu, self.weight_rho)
+
+    bias_mu = torch.empty(out_channels * 2,)
+    nn.init.constant_(bias_mu, 0.0)
+    self.bias_mu = nn.Parameter(bias_mu)
+    self.bias_rho = nn.Parameter(torch.Tensor(out_channels * 2,).uniform_(-5, -4))
+    self.bias = Gaussian(self.bias_mu, self.bias_rho)
+
+    self.weight_prior = Gaussian(torch.zeros_like(self.weight_mu), torch.ones_like(self.weight_rho)*0.542)
+    self.bias_prior = Gaussian(torch.zeros_like(self.bias_mu), torch.ones_like(self.bias_rho)*0.542)
+
+    self.log_prior = 0.0
+    self.log_variational_posterior = 0.0
+
+  def forward(self, x: torch.tensor, sample: bool=False)->torch.tensor:
+    if self.training or sample:
+      weight = self.weight.sample()
+      bias = self.bias.sample()
+    else:
+      weight = self.weight.mu
+      bias = self.bias.mu
+    if self.training:
+      self.log_prior = self.weight_prior.log_prob(weight) + self.bias_prior.log_prob(bias)
+      self.log_variational_posterior = self.weight.log_prob(weight) + self.bias.log_prob(bias)
+    else:
+      self.log_prior, self.log_variational_posterior = 0.0, 0.0
+    out = F.conv2d(x, weight, bias, self.stride, self.padding)
+    return out[:, :self.out_channels, :, :], out[:, self.out_channels:, :, :]
+
+
+class BBBLinear(nn.Module):
   def __init__(self,
                in_dim: int,
                out_dim: int):
@@ -54,7 +149,7 @@ class BBBLayer(nn.Module):
     return x @ weight + bias
 
 
-class BBBFinalLayer(nn.Module):
+class BBBLinearOutput(nn.Module):
   def __init__(self,
                in_dim: int,
                out_dim: int):
@@ -96,8 +191,8 @@ class BBB_FC(nn.Module):
     super().__init__()
     self.depth = len(dims)
     self.kl_weight = kl_weight
-    layers = [BBBLayer(dims[i], dims[i+1]) for i in range(len(dims) -2)]
-    layers.append(BBBFinalLayer(dims[-2], dims[-1]))
+    layers = [BBBLinear(dims[i], dims[i+1]) for i in range(len(dims) -2)]
+    layers.append(BBBLinearOutput(dims[-2], dims[-1]))
     self.layers = nn.ModuleList(layers)
 
 
@@ -136,7 +231,7 @@ class BBB_FC(nn.Module):
     return loss
 
 
-class BBB_neuron(nn.Module):
+class BBBNeuron(nn.Module):
   def __init__(self):
     super().__init__()
     self.weight_mu = nn.Parameter(torch.Tensor(1,).uniform_(-0.2, 0.2))
